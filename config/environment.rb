23cc8ef5f2e6ecccae24f6825ab7920e83ff840b
Charlie Cuddy <charliecuddy4@gmail.com>
2016-05-06 04:38:01 +0000

    Try new port in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :port => 2525,
     :authentication => :plain,
     :user_name => ENV['charliephoto'],
     :password => ENV['password1'],
