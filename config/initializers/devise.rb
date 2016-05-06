ad88cffc2294e0c45a581811e099833c97e5b886
Charlie Cuddy <charliecuddy4@gmail.com>
2016-05-06 05:10:16 +0000

    change config.mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'charliecuddy4@gmail.com'
+  config.mailer_sender = 'PhotoApp-Do Not Reply'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
