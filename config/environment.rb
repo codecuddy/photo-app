6afe689a77e5d8d929ad76fa96050bdec0c1a707
Charlie Cuddy <charliecuddy4@gmail.com>
2016-05-06 04:26:27 +0000

    Update email in devise.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net', 
-    :port => '587', 
-    :authentication => :plain, 
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
     :user_name => ENV['charliephoto'],
     :password => ENV['password1'],
     :domain => 'heroku.com',
